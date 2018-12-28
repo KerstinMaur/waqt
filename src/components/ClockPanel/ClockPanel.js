ad8272c525623555637e4d50d1232b0511600937
Sarim Abbas <sarim.stays@gmail.com>
2018-12-28 20:23:16 +0500

    animation improvements 2

--- src/components/ClockPanel/ClockPanel.js
+++ src/components/ClockPanel/ClockPanel.js
@@ -12,8 +12,8 @@ class ClockPanel extends Component {
     render() {
 
         const ClockPosed = posed.div({
-            enter: { x: 50, opacity: 1 },
-            exit: { x: 0, opacity: 0 }
+            enter: { x: 0, opacity: 1 },
+            exit: { x: -50, opacity: 0 }
         })
 
         return (
